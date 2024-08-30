dee3a214bdc74783201b34a904301d11e7196e43
Evandro <dam450@duck.com>
2024-08-30 15:42:35 -0300

    chore: set cors middleware

--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
 import express, { json } from "express";
+import cors from "cors";
 
 const port = 3333;
 const app = express();
 app.use(json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+    optionsSuccessStatus: 204,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
